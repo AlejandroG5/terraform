9defbe764c9dcbeccbf22bcc42869ec34949e259
Alejandro Godoy <a.godoylee@gmail.com>
2022-08-23 00:18:22 -0500

    Done

---
+++ main.tf
@@ -0,0 +1,66 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_KEY_ID" {
+  type = string
+}
+
+provider "aws"{
+    region     = "us-west-1"
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_KEY_ID
+}
+
+resource "aws_vpc" "primary_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Primary-VPC"
+  }
+}
+
+resource "aws_internet_gateway" "IG-1" {
+ vpc_id = aws_vpc.primary_vpc.id
+ tags = {
+    Name = "Internet Gateway-1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1" {
+ vpc_id = aws_vpc.primary_vpc.id
+ cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "Public Subnet 1"
+  }
+}
+
+resource "aws_route_table" "primary-vpc-rt" {
+ vpc_id = aws_vpc.primary_vpc.id
+ route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IG-1.id
+ } 
+}
+
+resource "aws_route_table_association" "public-subnet1--routetable" {
+  subnet_id = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.primary-vpc-rt.id
+}
+
+resource "aws_instance" "serv1" {
+  ami           = "ami-0e4d9ed95865f3b40"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public-subnet-1.id
+  tags = {
+    Name = "Server1"
+  }
+}
+
+resource "aws_instance" "serv2" {
+  ami           = "ami-0e4d9ed95865f3b40"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public-subnet-1.id
+  tags = {
+    Name = "Server2"
+  }
+}
